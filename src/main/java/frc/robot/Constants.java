98a4eb5bcb866bbb8db3110b50db28c9cbc2e4e5
Zebras-HP <frcteam313@gmail.com>
2023-03-17 17:49:22 -0400

    313 -> 9148

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -44,7 +44,7 @@ public final class Constants {
      * 42 -> counts per revolutions
      * 360 -> conversion to degrees
      */
-    public final static double ARM_BEND_RADIUS_ENCODER = 42*80*2.8*360;
+    public final static double ARM_BEND_RADIUS_ENCODER = 42*70*360;
     public final static double ARM_EXTEND_DISTANCE_ENCODER = 42*4*3;
 
     public final static double ARM_BEND_LOWER_LIMIT = 0;
